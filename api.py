ae6f4f8a0949566d19a8f3108f3a713317d6eb3b
AmmarAzhad <ammar.arifin@gmail.com>
2023-03-13 15:32:11 +0800

    Tried some stuff

--- api.py
+++ api.py
@@ -10,35 +10,40 @@ from threading import Thread
 
 app = Flask(__name__)
 
-@app.route('/api', methods = ['GET'])
+@app.route('/api', methods = ['POST'])
 def detect_emotion():
-    image = request.files['images']
-    detectedImage = {}
-    img = Image.open(image.stream)
-    detected = detectEmotion(img)
-    encodedImage = base64.b64encode(detected.read())
-    detectedImage['output'] = encodedImage
-    return jsonify({detectedImage})
+    if request.method == 'POST':
+        file = request.files['image']
+        data = file.stream.read()
+        data = base64.b64encode(data).decode()
+        image = cv2.imread(file.filename)
+        #detectedImage = {}
+        detected = detectEmotion(image)
+        encodedImage = base64.b64encode(detected)
+        encoded_str = encodedImage.decode("ascii")
+        #detectedImage['output'] = encodedImage
+        #encodedImage = base64.b64encode(image.read())
+        return jsonify({'output': encoded_str})
 
 if __name__ =='__main__':
     app.run()
 
-def detectEmotion(frame):
+def detectEmotion(image):
 
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    result = df.analyze(frame, actions = ['emotion'], enforce_detection=False)
+    result = df.analyze(img_path = 'C:\\Users\\User\\Downloads\\WhatsApp Image 2023-03-09 at 23.28.53.jpeg', actions = ['emotion'], enforce_detection=False)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(gray,1.1,4)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-    cv2.putText(frame,
+    cv2.putText(image,
                 result['dominant_emotion'],
                 (50,50),
                 font, 3,
@@ -46,4 +51,4 @@ def detectEmotion(frame):
                 2,
                 cv2.LINE_4)
 
-    return frame
+    return image
